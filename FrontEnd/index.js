e059475818663f24148ff83fda20b1075605ac64
NathanMachrouh <machrouh.nathan@gmail.com>
2023-10-04 17:26:24 +0200

    IW-49
    CSS de la page login

---
+++ FrontEnd/index.js
@@ -0,0 +1,56 @@
+const urlWorks = 'http://localhost:5678/api/works';
+const fetchWork = async (url, callback) =>
+{
+    const dataFetch = await fetch(url);
+    const data = await dataFetch.json();
+    callback(data);
+};
+
+const createGallery = (data) => {
+    const gallery = document.querySelector(".gallery");
+    gallery.innerHTML = null;
+    for (const work of data) {
+        const figure = document.createElement("figure");
+        figure.innerHTML = `
+        <img src = "${work.imageUrl}" alt = "${work.title}">
+        <figcaption>${work.title}</figcaption>
+        `;
+        gallery.appendChild(figure);
+    };
+};
+
+const createCategorie = (data) => {
+    const filtre = document.querySelector (".filtre");
+    const tous = document.createElement ("button");
+    tous.innerHTML = "Tous";
+    tous.addEventListener ("click", () => 
+    fetchWork(urlWorks, createGallery));
+    filtre.appendChild(tous);
+    for(const categorie of data){
+        const bouton = document.createElement("button");
+        bouton.innerHTML = categorie.name;
+        bouton.addEventListener ("click", () => 
+        fetchCategorieFilter(urlWorks, createGallery, categorie.id));
+        filtre.appendChild(bouton);
+    }
+
+};
+
+const urlCategorie = "http://localhost:5678/api/categories" ;
+const fetchCategorie = async (url, callback) =>
+{
+    const dataFetch = await fetch(url);
+    const data = await dataFetch.json();
+    callback(data);
+};
+
+const fetchCategorieFilter = async (url, callback, btnId) =>
+{
+    const dataFetch = await fetch(url);
+    let data = await dataFetch.json();
+    data = await data.filter((categorie) => categorie.categoryId === btnId);
+    callback(data);
+};
+
+fetchWork(urlWorks, createGallery);
+fetchCategorie(urlCategorie, createCategorie);
